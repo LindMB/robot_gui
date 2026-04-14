7a98effe0efbee7d871b8ba3bcd51be1f95d778b
Linda Mbongue <linda.mbongue@outlook.fr>
2026-04-14 22:53:40 +0000

    Move the robot position displays up

--- src/robot_position_area_class.cpp
+++ src/robot_position_area_class.cpp
@@ -22,24 +22,24 @@ void CVUIROSRobotPositionArea::odom_callback(
 void CVUIROSRobotPositionArea::draw(cv::Mat &frame, const int &width) {
 
   // Robot Position Area Title
-  cvui::printf(frame, 10, 630, 0.4, 0xffffff,
+  cvui::printf(frame, 10, 505, 0.4, 0xffffff,
                "Estimated robot position based off odometry");
 
   // X Position Window
-  cvui::window(frame, 10, 650, (width / 3 - 20), 120, "X");
+  cvui::window(frame, 10, 525, (width / 3 - 20), 120, "X");
   // Y Position Window
-  cvui::window(frame, (width / 3 + 10), 650, (width / 3 - 20), 120, "Y");
+  cvui::window(frame, (width / 3 + 10), 525, (width / 3 - 20), 120, "Y");
   // Z Position Window
-  cvui::window(frame, ((width / 3) * 2 + 10), 650, (width / 3 - 20), 120, "Z");
+  cvui::window(frame, ((width / 3) * 2 + 10), 525, (width / 3 - 20), 120, "Z");
 
   // Show x position inside the window.
-  cvui::printf(frame, 20, 710, 0.8, 0xffffff, this->x_pos.c_str());
+  cvui::printf(frame, 20, 585, 0.8, 0xffffff, this->x_pos.c_str());
 
   // Show Y position inside the window.
-  cvui::printf(frame, (width / 3 + 20), 710, 0.8, 0xffffff,
+  cvui::printf(frame, (width / 3 + 20), 585, 0.8, 0xffffff,
                this->y_pos.c_str());
 
   // Show Z position inside the window.
-  cvui::printf(frame, ((width / 3) * 2 + 20), 710, 0.8, 0xffffff,
+  cvui::printf(frame, ((width / 3) * 2 + 20), 585, 0.8, 0xffffff,
                this->z_pos.c_str());
 }
